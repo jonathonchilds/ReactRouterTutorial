5311d004277e1301e8c6be48ddd2bc388c157c0c
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2023-01-05 22:43:36 -0700

    adds an action on a route

---
+++ src/error-page.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useRouteError } from 'react-router-dom'
+
+export default function ErrorPage() {
+  const error = useRouteError()
+  console.error(error)
+
+  return (
+    <>
+      <div id="error-page">
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>
+            {
+              // @ts-ignore
+              error.statusText || error.message
+            }
+          </i>
+        </p>
+      </div>
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
-import Root from './routes/root'
+import Root, { loader as rootLoader, action as rootAction } from './routes/root'
+import ErrorPage from './error-page'
+import Contact from './routes/contact'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: 'contacts/:contactId',
+        element: <Contact />,
+      },
+    ],
   },
 ])
-// @ts-ignore
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-)
